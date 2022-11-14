cc4fdbe2747bae5686a957a62b57daeb2c5cc76f
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2022-11-14 20:33:21 +0000

    add feature to leave mission

--- src/components/OneMission/OneMission.js
+++ src/components/OneMission/OneMission.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
-import { joinMission } from '../../redux/missions/missions';
+import { joinMission, leaveMission } from '../../redux/missions/missions';
 import styles from './onemission.module.css';
 
 const OneMission = (props) => {
   const {
-    name, description, id,
+    name, description, id, reserved,
   } = props;
 
   const dispatch = useDispatch();
@@ -17,7 +17,15 @@ const OneMission = (props) => {
       <td className={styles.onemission_description}>{ description }</td>
       <td className={styles.notmember}><span>Not A Member</span></td>
       <td className={styles.joinmission}>
-        <button onClick={() => dispatch(joinMission(id))} type="button">Join Mission</button>
+        {
+          !reserved ? (
+            <button onClick={() => dispatch(joinMission(id))} type="button">Join Mission</button>
+          )
+            : (
+              <button onClick={() => dispatch(leaveMission(id))} type="button">Leave Mission</button>
+            )
+        }
+
       </td>
     </>
   );
@@ -26,7 +34,7 @@ OneMission.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
-  // reserved: PropTypes.bool.isRequired,
+  reserved: PropTypes.bool.isRequired,
 };
 
 export default OneMission;
