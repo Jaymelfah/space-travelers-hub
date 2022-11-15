5656b31a5bb16f189b0fb6ac45e0b1d66c465a48
KareemWilson <kareemmagdywilson@gmail.com>
2022-11-15 16:55:39 +0200

    Toggle rocket reservation buttons

--- src/components/RocketCard/RocketCard.jsx
+++ src/components/RocketCard/RocketCard.jsx
@@ -1,25 +1,48 @@
 import React from 'react';
-import PropTypes, { string } from 'prop-types';
+import { useDispatch } from 'react-redux';
+import PropTypes, { number, string } from 'prop-types';
+import { reservedRocket, canceledRocket } from '../../redux/rockets/rockets';
 import styles from './rocketCard.module.css';
 
 function RocketCard({ rocket }) {
-  const { name, description, imgs } = rocket;
+  const {
+    name,
+    description,
+    imgs,
+    id,
+  } = rocket;
+  const dispatch = useDispatch();
   return (
     <div className={styles.rocketCardContainer}>
       <img src={imgs[0]} alt="rocketImg" className={styles.rocketImg} />
       <div className={styles.rocketDetails}>
         <h5 className={styles.rocketTitle}>{name}</h5>
         <p className={styles.rocketDescription}>{description}</p>
-        <button type="button" className={styles.reserveRocketBtn}>
-          Reserve Rocket
-        </button>
+        {!rocket.reserved && (
+          <button
+            type="button"
+            className={styles.reserveRocketBtn}
+            onClick={() => dispatch(reservedRocket(id))}
+          >
+            Reserve Rocket
+          </button>
+        )}
+        {rocket.reserved && (
+          <button
+            type="button"
+            className={styles.cancelRocketBtn}
+            onClick={() => dispatch(canceledRocket(id))}
+          >
+            Cancel Reservation
+          </button>
+        )}
       </div>
     </div>
   );
 }
 
 RocketCard.propTypes = {
-  rocket: PropTypes.objectOf(string).isRequired,
+  rocket: PropTypes.objectOf(string, number).isRequired,
 };
 
 export default RocketCard;
