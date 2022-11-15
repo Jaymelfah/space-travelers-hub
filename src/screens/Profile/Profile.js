9f9117e11e24e45622d2c4633f0174057717dc7e
KareemWilson <kareemmagdywilson@gmail.com>
2022-11-15 18:11:00 +0200

    Display reserved Rockets in profile

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -4,11 +4,17 @@ import styles from './profile.module.css';
 
 function Profile() {
   const missions = useSelector((state) => state.mission);
+  const rockets = useSelector((state) => state.rockets);
 
-  // Remove border when filtered array is empty
   const arr = missions.filter((mission) => mission.reserved);
+  const rocketsReserved = rockets.filter((rocket) => rocket.reserved);
+  // Remove border when filtered array is empty
   const styling = {
     border: arr.length === 0 ? 'none' : '1px solid #e3e3e3',
+  };
+
+  const reservedRocketsstyling = {
+    border: rocketsReserved.length === 0 ? 'none' : '1px solid #e3e3e3',
   };
 
   return (
@@ -28,6 +34,14 @@ function Profile() {
 
       <div className={styles.rockets_container}>
         <h1>Rockets</h1>
+        {rocketsReserved.length === 0 && <p>No Rockets reserved yet...</p>}
+        <div style={reservedRocketsstyling}>
+          {rocketsReserved.map((rocket) => (
+            <div className={styles.reservedRocket} key={rocket.id}>
+              <span>{rocket.name}</span>
+            </div>
+          ))}
+        </div>
       </div>
 
     </div>
