29b34f995823786dc381bf77f6a53820218e1f22
KareemWilson <kareemmagdywilson@gmail.com>
2022-11-15 16:31:59 +0200

    Revert "Create"
    This reverts commit 73c7f660623e875922961b71a6f3b085f705e96a.

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,8 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const GET_ROCKETS = 'space-travelers-hub/rockets/GET_ROCKETS';
-const ROCKET_RESERVED = 'space-travelers-hub/rockets/ROCKET_RESERVED';
-const ROCKET_CANCELED = 'space-travelers-hub/rockets/ROCKET_CANCELED';
 
 const initialState = [];
 
@@ -29,41 +27,11 @@ export const getRockets = createAsyncThunk(GET_ROCKETS, async () => {
   }
 });
 
-export const rocketReserved = (payload) => ({
-  type: ROCKET_RESERVED,
-  payload,
-});
-
-export const rocketCanceled = (payload) => ({
-  type: ROCKET_CANCELED,
-  payload,
-});
-
 // Reducer
 const rocketsReducer = (state = initialState, action) => {
   switch (action.type) {
     case `${GET_ROCKETS}/fulfilled`:
       return action.payload;
-    case ROCKET_RESERVED:
-      return state.map((rocket) => {
-        if (rocket.id === action.payload) {
-          return {
-            ...rocket,
-            reserved: true,
-          };
-        }
-        return rocket;
-      });
-    case ROCKET_CANCELED:
-      return state.map((rocket) => {
-        if (rocket.id === action.payload) {
-          return {
-            ...rocket,
-            reserved: false,
-          };
-        }
-        return rocket;
-      });
     default:
       return state;
   }
